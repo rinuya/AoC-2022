aa8c88374fe45cfdddeb6f93244fb46799cbdaa2
rinuya <rinuya@users.noreply.github.com>
2022-12-11 16:39:13 +0100

    Day5 Part2

--- day5/day5.c
+++ day5/day5.c
@@ -2,22 +2,25 @@
 #include <string.h>
 
 typedef struct Stack {
-    char data[50];
+    char data[100];
     int top;
 } Stack;
 
-void push_on_stack(Stack* target, char value) {
-    target->top++;
-    target->data[target->top] = value;
+void push_on_stack(Stack* target, int amount, char* value) {
+    for (int i = 0; i < amount; i++){
+        target->top++;
+        target->data[target->top] = value[i];
+    } 
 }
 
-char pop_from_stack(Stack* target) {
+void pop_from_stack(Stack* target, int amount, char* returnvalue) {
     if (target->top < 0) {
-        printf("Error, stack is empty\n\n\n");
-        return -1;
+        printf("Error, stack is empty\n");
     }
-    target->top--;
-    return target->data[target->top+1];
+    for (int i = 0; i < amount; i++) {
+        returnvalue[amount-i-1] = target->data[target->top];
+        target->top--;
+    }
 }
 
 void split_string_and_assign (char* str, Stack** stacks) {
@@ -37,62 +40,47 @@ void split_string_and_assign (char* str, Stack** stacks) {
     p = strtok(NULL, " ");
     sscanf(p, "%d", &int_to);
 
-    printf("We move %d from the struct %d to the struct %d\n", amt, int_from, int_to);
-    // printf("Check if I can access the stack: 1 element 3 : %c\n", stacks[0]->data[2]);
-    for (int i = 0; i < amt; i++) {
-        char value[1];
-        value[0] = pop_from_stack(stacks[int_from-1]);
-        printf("Popped value %c\n", value[0]);
-        push_on_stack(stacks[int_to-1], value[0]);
-    }
+    char value[100];
+    pop_from_stack(stacks[int_from-1], amt, value);
+    push_on_stack(stacks[int_to-1], amt, value);
 }
 
 void assign_starting_input (Stack* one, Stack* two, Stack* three, Stack* four, Stack* five, Stack* six, Stack* seven, Stack* eight, Stack* nine) {
-    char arr_one[4] = {'H', 'T', 'Z', 'D'};
+    char arr_one[12] = {'H', 'T', 'Z', 'D'};
     one->top = -1;
-    for (int i=0; i < sizeof(arr_one); i++) {
-        push_on_stack(one, arr_one[i]);
-    }
-    char arr_two[7] = {'Q', 'R', 'W', 'T', 'G', 'C', 'S'};
+    push_on_stack(one, 4, arr_one);
+
+    char arr_two[12] = {'Q', 'R', 'W', 'T', 'G', 'C', 'S'};
     two->top = -1;
-    for (int i=0; i < sizeof(arr_two); i++) {
-        push_on_stack(two, arr_two[i]);
-    }
-    char arr_three[8] = {'P', 'B', 'F', 'Q', 'N', 'R', 'C', 'H'};
+    push_on_stack(two, 7, arr_two);
+
+    char arr_three[12] = {'P', 'B', 'F', 'Q', 'N', 'R', 'C', 'H'};
     three->top = -1;
-    for (int i=0; i < sizeof(arr_three); i++) {
-        push_on_stack(three, arr_three[i]);
-    }
-    char arr_four[6] = {'L', 'C', 'N', 'F', 'H', 'Z'};
+    push_on_stack(three, 8, arr_three);
+
+    char arr_four[12] = {'L', 'C', 'N', 'F', 'H', 'Z'};
     four->top = -1;
-    for (int i=0; i < sizeof(arr_four); i++) {
-        push_on_stack(four, arr_four[i]);
-    }
-    char arr_five[5] = {'G', 'L', 'F', 'Q', 'S'};
+    push_on_stack(four, 6 ,arr_four);
+
+    char arr_five[12] = {'G', 'L', 'F', 'Q', 'S'};
     five->top = -1;
-    for (int i=0; i < sizeof(arr_five); i++) {
-        push_on_stack(five, arr_five[i]);
-    }
-    char arr_six[8] = {'V', 'P', 'W', 'Z', 'B', 'R', 'C', 'S'};
+    push_on_stack(five, 5, arr_five);
+
+    char arr_six[12] = {'V', 'P', 'W', 'Z', 'B', 'R', 'C', 'S'};
     six->top = -1;
-    for (int i=0; i < sizeof(arr_six); i++) {
-        push_on_stack(six, arr_six[i]);
-    }
-    char arr_seven[3] = {'Z', 'F', 'J'};
+    push_on_stack(six, 8, arr_six);
+
+    char arr_seven[12] = {'Z', 'F', 'J'};
     seven->top = -1;
-    for (int i=0; i < sizeof(arr_seven); i++) {
-        push_on_stack(seven, arr_seven[i]);
-    }
-    char arr_eight[7] = {'D', 'L', 'V', 'Z', 'R', 'H', 'Q'};
+    push_on_stack(seven, 3, arr_seven);
+
+    char arr_eight[12] = {'D', 'L', 'V', 'Z', 'R', 'H', 'Q'};
     eight->top = -1;
-    for (int i=0; i < sizeof(arr_eight); i++) {
-        push_on_stack(eight, arr_eight[i]);
-    }
-    char arr_nine[8] = {'B', 'H', 'G', 'N', 'F', 'Z', 'L', 'D'};
+    push_on_stack(eight, 7, arr_eight);
+
+    char arr_nine[12] = {'B', 'H', 'G', 'N', 'F', 'Z', 'L', 'D'};
     nine->top = -1;
-    for (int i=0; i < sizeof(arr_nine); i++) {
-        push_on_stack(nine, arr_nine[i]);
-    }
+    push_on_stack(nine, 8, arr_nine);
 }
 
 int main () {
@@ -100,15 +88,24 @@ int main () {
     Stack one, two, three, four, five, six, seven, eight, nine;
     Stack* Stacks[9] = {&one, &two, &three, &four, &five, &six, &seven, &eight, &nine};
     assign_starting_input(Stacks[0], Stacks[1], Stacks[2], Stacks[3], Stacks[4], Stacks[5], Stacks[6], Stacks[7], Stacks[8]);
+
     for ( ;; ) {
         char line[30];
         fgets(line, 30, fp);
         if (feof( fp )) break;
+
         split_string_and_assign(line, Stacks);
     }
     if (feof(fp)) {
-        printf("solution %c:%c:%c:%c:%c:%c:%c:", pop_from_stack(&one),  pop_from_stack(&two),  pop_from_stack(&three),  pop_from_stack(&four),  pop_from_stack(&five),  pop_from_stack(&six),  pop_from_stack(&seven));
-        printf("%c:%c\n", pop_from_stack(&eight), pop_from_stack(&nine));
+        printf("1 || %c\n", one.data[one.top]);
+        printf("2 || %c\n", two.data[two.top]);
+        printf("3 || %c\n", three.data[three.top]);
+        printf("4 || %c\n", four.data[four.top]);
+        printf("5 || %c\n", five.data[five.top]);
+        printf("6 || %c\n", six.data[six.top]);
+        printf("7 || %c\n", seven.data[seven.top]);
+        printf("8 || %c\n", eight.data[eight.top]);
+        printf("9 || %c\n", nine.data[nine.top]);
     }
     fclose(fp);
     return 0;
