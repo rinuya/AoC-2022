9984ec23df661dc23e4302c66d0784de42a8ae8d
rinuya <rinuya@users.noreply.github.com>
2022-12-19 20:50:49 +0100

    Day10 Part2

--- day10/day10.c
+++ day10/day10.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+int GRID_VALUE = 10;
 
-void check_and_add_signal (int* array, int* index, int current_register, int cycle_count) {
-    if (cycle_count == 20 || ((cycle_count-20) % 40 == 0)) {
-        array[*index] = (current_register * cycle_count);
-        (*index)++;
+void check_and_switch_row (int* cycle, int* row) {
+    if ((*cycle) % 40 == 0) {
+        (*cycle) = 0;
+        (*row)--;
+    }
+}
+
+void add_pixel (int grid[40][6], int x_register, int pos_x, int row) {
+    if (abs(x_register - pos_x) <= 1) {
+        grid[pos_x][row] = GRID_VALUE;
     }
 }
 
 int main () {
     FILE* fp = fopen("input.txt", "r");
-    int cycle_count = 0, result = 0, x_register = 1, index = 0;
-    int signal_strengths[6]; 
+    int cycle_count = 0, row = 5, x_register = 1;
+    int grid[40][6]; 
 
     for ( ;; ) {
         char line[10];
@@ -21,21 +28,32 @@ int main () {
         char noop_or_addx[4];
         int line_amount;
         sscanf(line, "%s %d", noop_or_addx, &line_amount);
+
         if (noop_or_addx[0] == 'n'){
+            add_pixel(grid, x_register, cycle_count, row);
             cycle_count++;
-            check_and_add_signal(&signal_strengths, &index, x_register, cycle_count);
+            check_and_switch_row(&cycle_count, &row);
         } else {
+            add_pixel(grid, x_register, cycle_count, row);
             cycle_count++;
-            check_and_add_signal(&signal_strengths, &index, x_register, cycle_count);
+            check_and_switch_row(&cycle_count, &row);
+
+            add_pixel(grid, x_register, cycle_count, row);
             cycle_count++;
-            check_and_add_signal(&signal_strengths, &index, x_register, cycle_count);
+            check_and_switch_row(&cycle_count, &row);
             x_register += line_amount;
         }
     }
-    for (int i = 0; i < index; i++) {
-        result += signal_strengths[i];
+    for (int i = 5; i >= 0; i--){
+        for (int j = 0; j<40; j++){
+            if (grid[j][i] == GRID_VALUE) {
+                printf("#");
+            } else {
+                printf(".");
+            }
+        }
+        printf("\n");
     }
-    printf("Result is: %d", result);
     fclose(fp);
     return 0;
 }
