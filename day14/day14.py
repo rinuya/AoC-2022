f349ad3ff1ccaff42b73e74c9d2bef60936c7764
rinuya <rinuya@users.noreply.github.com>
2023-01-22 13:57:45 +0100

    Day14 Part2 (Python)

--- day14/day14.py
+++ day14/day14.py
@@ -1,18 +1,15 @@
 class Map():
-    def __init__(self) -> None:
+    def __init__(self):
         self.grid = [['.' for el in range(1000)] for row in range(500)]
         self.sand_amt = 0
-        self.sand_row = 0
-        self.sand_col = 500
+        self.sand_row = None
+        self.sand_col = None
         self.lowest_point = 1
-        self.end = False
 
-
-    def addHashtag(self, l):
+    def add_rocks(self, l):
         pos = 0
         while pos + 1 < len(l):
-            xy1 = list(map(int, l[pos].split(',')))
-            xy2 = list(map(int, l[pos+1].split(',')))
+            xy1, xy2 = list(map(int, l[pos].split(','))), list(map(int, l[pos+1].split(',')))
 
             if xy1[1] > self.lowest_point:
                 self.lowest_point = xy1[1]
@@ -21,51 +18,49 @@ class Map():
 
             if xy1[0] == xy2[0]:
                 hashes = sorted([xy1[1], xy2[1]])
-                for i in range(hashes[1]-hashes[0]+1):
+                for i in range(hashes[1] - hashes[0] + 1):
                     self.grid[hashes[0]+i][xy1[0]] = '#'
             else:
                 hashes = sorted([xy1[0], xy2[0]])
-                for i in range(hashes[1]-hashes[0]+1):
+                for i in range(hashes[1] - hashes[0] + 1):
                     self.grid[xy1[1]][hashes[0]+i] = '#'
 
             pos = pos+1
 
-    def dropSand(self):
-        if self.sand_row > self.lowest_point:
-            self.end = True
-            self.sand_row = None
-            self.sand_col = None
+    def add_ground_level(self):
+        row_level = self.lowest_point + 2
+        for i in range(len(self.grid[row_level])):
+            self.grid[row_level][i] = '#'
 
-        elif self.grid[self.sand_row+1][self.sand_col] == '.': 
-            self.sand_row = self.sand_row + 1
-
-        elif self.grid[self.sand_row + 1][self.sand_col - 1] == '.':    
+    def drop_sand(self):
+        if self.grid[self.sand_row+1][self.sand_col] == '.':
+            self.sand_row = self.sand_row + 1                       
+        elif self.grid[self.sand_row + 1][self.sand_col - 1] == '.': 
             self.sand_row = self.sand_row + 1
             self.sand_col = self.sand_col - 1
-
-        elif self.grid[self.sand_row + 1][self.sand_col + 1] == '.':
+        elif self.grid[self.sand_row + 1][self.sand_col + 1] == '.':    
             self.sand_row = self.sand_row + 1
             self.sand_col = self.sand_col + 1
-
         else:
             self.grid[self.sand_row][self.sand_col] = 'o'
             self.sand_row = None
             self.sand_col = None
 
-    def sandMove(self):
-        while self.end == False:
-            self.sand_row, self.sand_col, self.sand_amt = 1, 500, self.sand_amt + 1
+    def start_sand_move(self):
+        while self.grid[0][500] == '.':
+            self.sand_row, self.sand_col, self.sand_amt = 0, 500, self.sand_amt + 1
             while self.sand_row != None and self.sand_col != None:
-                self.dropSand()
-             
-        print(f"The solution is:{self.sand_amt -1}")
+                self.drop_sand()
+    
+        print(f"The solution is: {self.sand_amt}")
 
 
-wf = Map()
+water_fall = Map()
 f = open("input.txt", "r")
 line_arr = [line[0:-1].split(' -> ') for line in f.readlines()]
 f.close()
 for line in line_arr:
-    wf.addHashtag(line)
+    water_fall.add_rocks(line)
 
-wf.sandMove()
+water_fall.add_ground_level()
+water_fall.start_sand_move()
